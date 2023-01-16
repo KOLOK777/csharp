d92304d59238ba8a45ab0d90b7a10fe293f01f51
KOLOK777 <kolevg3@gmail.com>
2023-01-17 00:00:31 +0300

    скорпировано решение с прошлой задачи для примера написания

--- task6/Program.cs
+++ task6/Program.cs
@@ -3,3 +3,23 @@
 //4 -> да
 //-3 -> нет
 //7 -> нет
+
+Console.Writeline("Введите первое число")
+int number1 = convert.ToInt(Console.ReadLine())
+Console.Writeline("Введите второе число")
+int number2 = convert.ToInt(Console.ReadLine())
+
+if (number1 == number2)
+{
+Console.Writeline("числа одинаковые !")
+}
+    if (number1 > number2) 
+{
+        Console.Writeline("бОльшее число: " + number1)
+        Console.Writeline("мЕньшее число: " + number2)
+}
+    else 
+{
+        Console.Writeline("бОльшее число: " + number2)
+        Console.Writeline("мЕньшее число: " + number1)
+}

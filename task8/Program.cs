1ad56646b470d8a579f7150177920687ebcdd92a
KOLOK777 <kolevg3@gmail.com>
2023-01-17 00:01:12 +0300

    скорпировано решение с прошлой задачи для примера написания

--- task8/Program.cs
+++ task8/Program.cs
@@ -2,3 +2,23 @@
 
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
+
+Console.Writeline("Введите первое число")
+int number1 = convert.ToInt(Console.ReadLine())
+Console.Writeline("Введите второе число")
+int number2 = convert.ToInt(Console.ReadLine())
+
+if (number1 == number2)
+{
+Console.Writeline("числа одинаковые !")
+}
+    if (number1 > number2) 
+{
+        Console.Writeline("бОльшее число: " + number1)
+        Console.Writeline("мЕньшее число: " + number2)
+}
+    else 
+{
+        Console.Writeline("бОльшее число: " + number2)
+        Console.Writeline("мЕньшее число: " + number1)
+}

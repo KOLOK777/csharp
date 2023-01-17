ff922c5ca31350ba3d5cac07a5bd5ceceb5cdc55
KOLOK777 <kolevg3@gmail.com>
2023-01-17 23:45:17 +0300

    черновик

--- task13/Program.cs
+++ task13/Program.cs
@@ -2,3 +2,18 @@
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int bit = 1000;
+
+if (number < 100 && number > -100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+while (number / bit == 0)
+        bit = bit * 10;
+        
+Console.WriteLine(bit);
+int digit = number % (bit / 100);       
+Console.WriteLine(digit);

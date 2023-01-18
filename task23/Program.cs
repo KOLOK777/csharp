5ac3e589f237736d4373eac445b81fe2f5128b13
KOLOK777 <kolevg3@gmail.com>
2023-01-18 17:00:52 +0300

    ввод условий

---
+++ task23/Program.cs
@@ -0,0 +1,9 @@
+﻿// 
+
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int number;
+while (!Int32.TryParse(Console.ReadLine(), out number) || number > 7 || number < 1)
+    {
+    Console.WriteLine("Ввdод неверный. Пожалуйста, введите корректное число");
+    }

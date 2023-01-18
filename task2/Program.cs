a2e7cdc21d674d64f84243f634958332b4df3b45
KOLOK777 <kolevg3@gmail.com>
2023-01-18 12:32:45 +0300

    финал с проверкой на текст/целое число

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,23 +3,32 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
-
+         
 Console.WriteLine("Введите первое число");
-int number1 = Convert.ToInt32(Console.ReadLine());
+int number1;
+while (!Int32.TryParse(Console.ReadLine(), out number1))
+{   
+    Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+}
 Console.WriteLine("Введите второе число");
-int number2 = Convert.ToInt32(Console.ReadLine());
+int number2;
+while (!Int32.TryParse(Console.ReadLine(), out number2))
+{   
+    Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+}
 
 if (number1 == number2)
 {
 Console.WriteLine("числа одинаковые !");
 }
+ else
     if (number1 > number2) 
-{
+    {
         Console.WriteLine("бОльшее число: " + number1);
         Console.WriteLine("мЕньшее число: " + number2);
-}
+    }
     else 
-{
+    {
         Console.WriteLine("бОльшее число: " + number2);
         Console.WriteLine("мЕньшее число: " + number1);
-}
+    }

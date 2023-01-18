c6c6e9fbc5bfd1e79c6dd86602a01c00920042df
KOLOK777 <kolevg3@gmail.com>
2023-01-18 13:43:20 +0300

    финал с проверкой на текст/целое число

--- task8/Program.cs
+++ task8/Program.cs
@@ -3,20 +3,44 @@
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
 
-Console.Write("Введите число N (больше 1): ");
-int N = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N;
+while (!Int32.TryParse(Console.ReadLine(), out N))
+    {   
+    Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+    }
+int helpN = N;
 int count = 4;
 
-Console.WriteLine("ИТОГ:");
+Console.WriteLine("ИТОГ (выводим четные числа):");
 
-if (N <= 1)
-{
-Console.WriteLine("в промежутке нет четных чисел !");    
-}
-else
+if (N == 1)
+    {
+    Console.WriteLine("в промежутке нет четных чисел !");       
+    }
+if (N > 1) 
+    {    
     Console.Write(count-2);
+    }
     while (count <= N)
     {
-        Console.Write(", " + count);
-        count = count + 2;
+    Console.Write(", " + count);
+    count = count + 2;
     }
+if (N < -2)
+    helpN = N;
+    if (N % 2 !=0)
+        {
+        helpN = N + 1;
+        }       
+    while (helpN <= 0)
+        if (helpN != 0)
+            {
+            Console.Write(helpN + ", ");
+            helpN = helpN + 2;
+            }
+        else
+            {
+            Console.Write(helpN);
+            helpN = helpN + 2;
+            }

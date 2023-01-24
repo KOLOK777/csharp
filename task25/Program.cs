a96eb7e0dc25b180514e40aaa99129b1ff8b6b43
KOLOK777 <kolevg3@gmail.com>
2023-01-24 22:31:23 +0300

    финальная версия

--- task25/Program.cs
+++ task25/Program.cs
@@ -2,3 +2,20 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
+int number;
+int power;
+Console.WriteLine("Введите целое число, которое будем возводить в степень");
+while (!Int32.TryParse(Console.ReadLine(number), out number))
+    Console.WriteLine("Неверный ввод. Введите правильное целое число");
+Console.WriteLine($"Введите степень, в которую будем возводить число {number}");
+while (!Int32.TryParse(Console.ReadLine(), out power))
+    Console.WriteLine("Неверный ввод. Введите правильное целое число");
+
+double raisedPower()
+    {
+    double result = Math.Pow(number, power);
+    Console.WriteLine($"Результат возвдения числа {number} в степень {power} : {result}");
+    return result;
+    }
+
+raisedPower ();

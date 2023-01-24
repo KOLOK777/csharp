57fbd992310731b11196857a3d81b39b33455433
KOLOK777 <kolevg3@gmail.com>
2023-01-24 13:15:41 +0300

    промежуточный вариант

--- task27/Program.cs
+++ task27/Program.cs
@@ -3,3 +3,31 @@
 // 82 -> 10
 // 9012 -> 12
 
+void SUM (int[] array)
+    {
+    Console.WriteLine("Введите целое число");
+    int number;
+    while (!Int32.TryParse(Console.ReadLine(), out number))
+        Console.WriteLine("Неверный ввод. Введите правильное целое число");
+    int[] array = new int[1];
+    int i = 0;
+    array[0] = number % 10;
+    int result = array[0];
+    int number2 = number / 10;
+        Console.Write($"Массив: [ {array[0]}");
+    for (i = 1; number2 > 0; i++)
+        {
+        array = new int[array.Length + 1];
+        array[i] = number2 % 10;
+        number2 = number2 / 10;
+        result = result + array[i];
+        Console.Write($", {array[i]}");
+        }
+    Console.Write(" ]");
+    Console.WriteLine();
+    Console.WriteLine($"Сумма цифр в вашем числе: {result}");
+    return;    
+    }
+
+SUM (array);
+

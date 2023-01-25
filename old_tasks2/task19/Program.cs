b62930de6f54557e374b26895fb9451ec1f31b56
KOLOK777 <kolevg3@gmail.com>
2023-01-25 17:42:08 +0300

    убрал в OLD

---
+++ old_tasks2/task19/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом 
+//(НЕ использовать число как строку, то есть сравнения типа number[0] == number[4] делать НЕЛЬЗЯ. 
+//Используем операторы % и /).
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number;
+while(!Int32.TryParse(Console.ReadLine(), out number) || (number < 10000 && number > -10000) || number >= 100000 || number <= -100000 )
+    Console.WriteLine("Введено неверно. Введите пятизначное число: ");
+
+void Palindrom (int number)
+    {
+    bool testTrue;
+    if ((number / 10000 == number % 10) && (number / 1000 % 10 == number / 10 % 10))
+        testTrue = true;
+    else
+        testTrue = false;
+    if (testTrue == true)
+        Console.WriteLine($"Веведено пятизначное число: {number}, это палиндром - 'да'");
+    else
+        Console.WriteLine($"Веведено пятизначное число: {number}, это НЕ палиндром - 'нет'");
+    return;    
+    }
+    
+Palindrom (number);
